f4575517f6b7bcabf08fc7986e1f61a1f5ce4603
Dan Burzo <dan@danburzo.ro>
2021-03-07 11:17:26 +0200

    Improvements: copy text to clipboard, better mobile ux

--- src/components/TextOutput.js
+++ src/components/TextOutput.js
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 
 import './TextOutput.css';
+
+const has_async_clipboard = navigator.clipboard && navigator.clipboard.writeText;
 
 const TextOutput = ({ results }) => {
 	if (!results || !results.length) {
@@ -14,6 +16,16 @@ const TextOutput = ({ results }) => {
 			return `[Error processing, check JSON output]`;
 		})
 		.join('\n§§\n');
+
+	const copyToClipboard = useCallback(
+		e => {
+			if (has_async_clipboard) {
+				navigator.clipboard.writeText(text);
+			}
+		},
+		[text]
+	);
+
 	return (
 		<div className="text-output">
 			<h2>Text output</h2>
@@ -21,6 +33,13 @@ const TextOutput = ({ results }) => {
 				Pages are separated with the <strong>§§</strong> sequence of characters.
 			</p>
 			<textarea value={text} readOnly />
+			{has_async_clipboard && (
+				<p>
+					<button type="button" onClick={copyToClipboard}>
+						Copy to clipboard
+					</button>
+				</p>
+			)}
 		</div>
 	);
 };
